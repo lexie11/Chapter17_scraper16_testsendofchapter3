040172181cab1902a24a11b8ff6c93ef5bca1fc4
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-08-06 09:10:46 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,45 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+###############################################################################
+# START HERE: Tutorial 3: More advanced scraping. Shows how to follow 'next' 
+# links from page to page: use functions, so you can call the same code 
+# repeatedly. SCROLL TO THE BOTTOM TO SEE THE START OF THE SCRAPER.
+###############################################################################
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import urlparse
+import lxml.html
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# scrape_table function: gets passed an individual page to scrape
+def scrape_table(root):
+# define the function and call the parameter it will use (aka the cake "filling") as 'jim' - or any other name
+    rows = root.cssselect("TABLE TR")  # selects all <tr> blocks within <table class="Trolley table"> and puts in list variable 'rows'
+# tell the function what it will do with 'jim'
+#     My guess is it knows to make a list variable because either 'cssselect' function has that written in or it does it automatically because there are multiple table rows
+# If a class name has a space in it, replace this with a . --> "Trolley table" > "Trolley.table"
+    for row in rows:
+        # Set up our data record - we'll need it later
+        record = {}
+        table_cells = row.cssselect("TD")
+        if table_cells: 
+            record['Racecourse'] = table_cells[0].text_content()
+            record['Address and Phone Number'] = table_cells[1].text_content()
+            # Print out the data we've gathered
+            print record, '------------'
+            # Finally, save the record to the datastore - 'Artist' is our unique key
+            scraperwiki.sqlite.save(["Racecourse"], record)
+        
+# # scrape_and_look_for_next_link function: calls the scrape_table function
+def scrape_and_look_for_next_link(url):
+    html = scraperwiki.scrape(url)
+    print html
+    root = lxml.html.fromstring(html)
+# define the variable you plan to put into the scrape_table function as its argument (the actual "filling" e.g. custard or jam)
+    scrape_table(root)
+# when you call scrape_table choose what specific argument you're replacing the parameter (jim) with (root) - ie. custard
+
+# ---------------------------------------------------------------------------
+# START HERE: define your starting URL - then 
+# call a function to scrape the first page in the series.
+# ---------------------------------------------------------------------------
+
+starting_url = 'http://www.ukjockey.com/racecourses.html'
+scrape_and_look_for_next_link(starting_url)
